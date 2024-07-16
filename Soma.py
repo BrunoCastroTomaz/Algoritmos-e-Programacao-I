a7664f8b2e974eacef0f0f22ce8c7e619cd45db8
BrunoCastroTomaz <bruno.tomaz@mackenzista.com.br>
2024-07-16 18:10:41 -0300

    Adicionando os codigos desenvolvidos na primeira aula

---
+++ Soma.py
@@ -0,0 +1,8 @@
+'''
+Programa que calcula a soma de dois números
+'''
+
+num1= int(input("Digite o primeiro número: "))
+num2= int(input("Digite o segundo número: "))
+
+print(f"A soma desses números é {num1+num2}")
---
+++ exercicioDobroTriploQuadrado.py
@@ -0,0 +1,10 @@
+'''
+Descrição: Algoritmo que calcula o dobro, triplo e o quadrado de um número
+Autor: Bruno
+Data atual: 12/08/2021
+'''
+
+numero = int(input("Digite um número: "))
+print(f"O dobro desse número é {numero*2}")
+print(f"O triplo desse número é {numero*3}")
+print(f"O quadrado desse número é {numero**2}")
